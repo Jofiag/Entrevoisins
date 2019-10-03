71ffa19af173d2b0ca87a6bcd4740ab06f3c42bf
Jofiag <Aguemon.wahab.97@gmail.com>
2019-10-03 19:15:25 +0200

    Ajout de l'activité NeighbourDetailsActivity et de son fichier xml.

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/controlleur/NeighbourDetailsActivity.java
@@ -0,0 +1,17 @@
+package com.openclassrooms.entrevoisins.controlleur;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.openclassrooms.entrevoisins.R;
+
+public class NeighbourDetailsActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_neighbour_details);
+    }
+}
