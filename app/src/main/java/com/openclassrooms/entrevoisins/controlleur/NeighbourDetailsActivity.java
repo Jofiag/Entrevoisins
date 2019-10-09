000bd13c99de6c1d2c3e7a04a7d166c86ccc4a15
Jofiag <Aguemon.wahab.97@gmail.com>
2019-10-09 09:50:53 +0200

    Implémentation de l'interface Neighbour_details.

--- app/src/main/java/com/openclassrooms/entrevoisins/controlleur/NeighbourDetailsActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/controlleur/NeighbourDetailsActivity.java
@@ -1,17 +1,29 @@
 package com.openclassrooms.entrevoisins.controlleur;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.openclassrooms.entrevoisins.R;
+import com.openclassrooms.entrevoisins.model.Neighbour;
+import
 
 public class NeighbourDetailsActivity extends AppCompatActivity
 {
-
+    private ImageView mImageView;
+    private Neighbour mNeighbour;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_neighbour_details);
+
+        mNeighbour = new Neighbour();
+
+        mImageView = findViewById(R.id.avatar);
+
+        Image avatarImage = new Image(mNeighbour.getAvatarUrl());
+
     }
 }
