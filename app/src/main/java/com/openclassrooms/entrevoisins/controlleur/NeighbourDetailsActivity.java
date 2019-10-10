cd814a16852491317b0e85536a1657a41a1dfef7
Jofiag <Aguemon.wahab.97@gmail.com>
2019-10-10 10:22:16 +0200

    Référenciation des éléments graphiques.

--- app/src/main/java/com/openclassrooms/entrevoisins/controlleur/NeighbourDetailsActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/controlleur/NeighbourDetailsActivity.java
@@ -1,9 +1,12 @@
 package com.openclassrooms.entrevoisins.controlleur;
 
 import android.media.Image;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.openclassrooms.entrevoisins.R;
 import com.openclassrooms.entrevoisins.model.Neighbour;
@@ -11,11 +14,31 @@ import com.openclassrooms.entrevoisins.model.Neighbour;
 public class NeighbourDetailsActivity extends AppCompatActivity
 {
     private ImageView mImageView;
+    private ImageButton mBackButton;
+    private TextView mNeighbourNameOnAvatar;
+    private TextView mNeighbourNameOnCardView;
+    private FloatingActionButton mFavoriteButton;
+    private TextView mNeighbourAddress;
+    private TextView mNeighbourPhoneNumber;
+    private TextView mNeighbourFacebook;
+    private TextView mNeighbourMoreAbout;
+
     private Neighbour mNeighbour;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_neighbour_details);
+
+        mImageView = findViewById(R.id.neighbour_avatar_img);
+        mBackButton = findViewById(R.id.back_button_btn);
+        mNeighbourNameOnAvatar = findViewById(R.id.neighbour_name_in_avatar_txt);
+        mNeighbourNameOnCardView = findViewById(R.id.neighbour_name_in_cardv_txt);
+        mFavoriteButton = findViewById(R.id.favorite_button);
+        mNeighbourAddress = findViewById(R.id.neighbour_address_txt);
+        mNeighbourPhoneNumber = findViewById(R.id.phone_number_txt);
+        mNeighbourFacebook = findViewById(R.id.neighbour_web_txt);
+        mNeighbourMoreAbout = findViewById(R.id.about_details_txt);
     }
 }
